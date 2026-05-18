7d8b8425abbe7fb4fc5cd416ad4900365425eed1
Andreas Katzian <andreas.katzian@jademind.com>
2026-05-18 15:36:58 +0200

    chore: Update mailjet credentials

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "Mikiwa <no-reply@mikiwa.at>"
+  default from: "Mikiwa <no-reply@app.mikiwa.at>"
   layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: Rails.application.credentials.dig(:app, :host) || "mikiwa.at" }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.dig(:app, :host) || "app.mikiwa.at" }
   config.action_mailer.smtp_settings = {
     user_name: Rails.application.credentials.dig(:mailjet, :api_key),
     password:  Rails.application.credentials.dig(:mailjet, :secret_key),
